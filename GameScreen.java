bc0dac785730c65bbb5fb436b2d9b8a8c357e719
JCPosso <44146884+JCPosso@users.noreply.github.com>
2018-11-14 20:32:13 -0500

    Update GameScreen.java

--- GameScreen.java
+++ GameScreen.java
@@ -4,6 +4,7 @@ import java.awt.Graphics2D;
 import java.util.ArrayList;
 
 import aplicacion.*;
+
 import java.awt.Color;
 import java.awt.Font;
 
@@ -15,78 +16,31 @@ import java.awt.Color;
 import java.awt.Font;
 
 public class GameScreen  {
-	
-	private Player player;
+	private Jugador player;	
 	private Ovni naveInvasora;
-	private ArrayList<Alien> enemies = new ArrayList<Alien>();
+	private bloqueEnemigo bloqueClasico;
 	public static int SCORE = 0;
 	
-	private Font gameScreen = new Font("Arial", Font.PLAIN, 48);	
+	private Font gameScreen = new Font("Arial", Font.PLAIN, 50);	
 	
 	public GameScreen( ) {
 		naveInvasora = new Ovni(0,0);
-		addEnemies();
-
+		bloqueClasico =  new bloqueEnemigo(25,25,4,10);
+		player = new Jugador(Display.WIDTH,Display.HEIGHT);
 	}
 	
 	public void update(double delta) {
-		naveInvasora.update(delta, player);
-		updateAliens(delta);
+		naveInvasora.update(delta);
+		bloqueClasico.updateAliens(delta);
 	}
 	public void draw(Graphics2D g) {
 		g.setColor(Color.white);
-		g.drawString("Score: " + SCORE, 5, 15);
+		g.drawString("Score: " + SCORE, 15, 35);
 		
 		g.setColor(Color.red);
 		g.drawString("Health: ", 5, 35);
-		
+		player.draw(g);
 		naveInvasora.draw(g);
-		drawAliens(g);
-		}
-	
-	
-	
-	
-	
-	
-	public void addEnemies() {
-		for(int y = 0; y < 5; y++){
-			for(int x = 0; x < 10; x++){
-				Alien e = new Alien(150 + (x * 40), 25 + (y * 40));
-				enemies.add(e);
-			}
-		}
-	}
-	public void ChangeAliens(double delta) {
-		if(enemies == null)
-			return;
-		
-		for(int i = 0; i < enemies.size(); i++){
-			if(enemies.get(i).isOutOfBounds()){
-				enemies.get(i).changeDirection(delta);
-			}
-		}
-	}
-	public void updateAliens(double delta) {
-		if(enemies == null)
-			return;
-		
-		for(int i = 0; i < enemies.size(); i++){
-			enemies.get(i).update(delta, player);
-		}
-		ChangeAliens(delta);
-	}
-	public void drawAliens(Graphics2D g) {
-		if(enemies == null)
-			return;
-		
-		for(int i = 0; i < enemies.size(); i++){
-			enemies.get(i).draw(g);
-		}
-	}
-	public void init(Canvas canvas) {
-		
-		canvas.addKeyListener(player);
-	}
-
+		bloqueClasico.drawAliens(g);
+		}	
 }
